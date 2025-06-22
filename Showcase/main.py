5ec9028327dd731f0cb9abeb5052a259dff3f4d3
antoedison <edisonanto7@gmail.com>
2025-06-22 15:51:08 +0530

    Update main.py

--- Showcase/main.py
+++ Showcase/main.py
@@ -1,15 +1,22 @@
 # main.py
 import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
 
 st.sidebar.title("Project Navigator")
 selected_app = st.sidebar.radio("Choose a Service:", [
-    "Project 1", "Pdf Assistant"
+    "CGPA CALCULATOR", "Pdf Assistant", "Chatbot"
 ])
 
-if selected_app == "Project 1":
+if selected_app == "CGPA CALCULATOR":
     from CGPA import cgpa
-    cgpa.run()
+    cgpa.main()
 if selected_app == "Pdf Assistant":
     from Pdf_assistant import Pdf_assistant
     Pdf_assistant.main()
+if selected_app == "Chatbot":
+    from Chatbot import Chatbot
+    Chatbot.main()
+
 
